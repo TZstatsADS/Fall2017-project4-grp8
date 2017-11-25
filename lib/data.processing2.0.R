9958dc5dd638e176597491cbe676be96967a9bb0
Sijian Xuan <sijianxuan@SijianmatoAir.fios-router.home>
2017-11-25 00:40:19 -0500

    add movietrain 2.0

--- lib/data.processing2.0.R
+++ lib/data.processing2.0.R
@@ -1,6 +1,7 @@
 #######author: SijianXuan#########################
 # make adject list data for webtrain and movietrain
 library(recommenderlab)
+################ web train ##########################
 webtrain = read.csv("../data/MS_sample/data_train.csv")
 ############################################################
 nrow = sum(webtrain[,2] == 'C')
@@ -39,3 +40,17 @@ rownames(web.train.matrix) = web.feature
 #####################################################
 save(web.train.matrix,file="../output/web.train2.0.Rdata")
 write.csv(web.train.matrix,file = "../data/web.train2.0.csv")
+################# movie train #######################
+movietrain = read.csv("../data/eachmovie_sample/data_train.csv")
+movietrain = movietrain[,-1]
+
+nrow_movie = length(unique(movietrain[,2]))
+ncol_movie = length(unique(movietrain[,1]))
+
+movie.train.matrix = matrix(NA, nrow = nrow_movie, ncol = ncol_movie)
+
+################ write matrix #####################
+movie.train.matrix = reshape(movietrain, idvar = "User", timevar = "Movie", direction = "wide")
+
+write.csv(movie.train.matrix,"../data/movie.train2.0.csv")
+ 
