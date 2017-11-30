55f7f88a2d093334feb51516d3db954c59df8d2f
Yiran Li <yl3538@columbia.edu>
2017-11-30 09:13:52 -0500

    Update rank_scoring.R

--- lib/rank_scoring.R
+++ lib/rank_scoring.R
@@ -3,10 +3,10 @@
 
 # spearman_p <- memory_based_prediction(train_data = web_train_rank0[1:num_users,], w = spearman_w[1:num_users,])
 # alpha ususally = 5
-rank_scoring <- function(predicted, web_mini_test, alpha= 5){
+rank_scoring <- function(predicted, web_mini_test, alpha){
   
   visited_ind <- apply(web_mini_test, 1, function(rrr){return(which(rrr==1))})
-  ranks <- apply(web_mini_test, 1, function(rrr){return(rank(rrr, ties.method = "random"))})
+  ranks <- apply(web_mini_test, 1, function(rrr){return(rank(-rrr, ties.method = "max"))})
   ord <- apply(predicted, 1, function(rrr){return(order(rrr))})
   # R_a_s: Expected utility of a ranked list for user a 
   R_a_s <- rep(NA, nrow(web_mini_test))
@@ -18,7 +18,7 @@ rank_scoring <- function(predicted, web_mini_test, alpha= 5){
   }
   
   # Final rank score for an experiment
-  R <- sum(R_a_s) / sum(R_a_max)
+  R <- sum(R_a_s) / sum(R_a_max)*100
   return(R)
 }
 ### END rank_scoring ###
